b8361e08aa7d4d1f05e213637955174b602b9f2a
nalendro16 <nalendro_agung@yahoo.com>
2021-12-27 00:14:20 +0700

    canceling to add document update on reducer

--- src/pages/home/TransactionList.js
+++ src/pages/home/TransactionList.js
@@ -2,7 +2,7 @@ import { useFirestore } from '../../hooks/useFirestore'
 import styles from './Home.module.css'
 
 export default function TransactionList({ transactions }) {
-  const { deleteDocument, response } = useFirestore('transactions')
+  const { deleteDocument } = useFirestore('transactions')
   return (
     <ul className={styles.transactions}>
       {transactions.map((transaction) => (
